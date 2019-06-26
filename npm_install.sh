5f9bdd063b77ef52885b10399eae6b1ef4089cbc
周立翊 <lzhou1110@gmail.com>
2019-06-26 15:37:11 +0100

    renamed folder from source to truffledrizzle

--- npm_install.sh
+++ npm_install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 rm -rf client/node_modules
 npm cache verify
-docker run --rm -v "$(pwd)/source/client":/data -w /data -it node:10 bash
+docker run --rm -v "$(pwd)/truffledrizzle/client":/data -w /data -it node:10 bash
