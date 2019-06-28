7d442d75ff76aa8c8a50d236ad03252e20afc6f9
周立翊 <lzhou1110@gmail.com>
2019-06-28 19:04:36 +0100

    Fix the path of node_modules, which need to be removed to garantee successful execution

--- npm_install.sh
+++ npm_install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-rm -rf client/node_modules
+rm -rf truffledrizzle/client/node_modules
 npm cache verify
 docker run --rm -v "$(pwd)/truffledrizzle/client":/data -w /data -it node:10 bash
